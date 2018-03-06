82138611f1b0608b591e4945e42cb037aa6b0e67
jfc3 <buckslice@gmail.com>
2018-03-05 21:00:34 -0800

    Added open file dialog and cleaned out old code

--- MagPy4.py
+++ MagPy4.py
@@ -1,6 +1,6 @@
 
 # python 3.6
-
+import os
 import sys
 from PyQt5 import QtGui, QtCore, QtWidgets
 from PyQt5.QtWidgets import QSizePolicy
@@ -37,8 +37,7 @@ class MagPy4Window(QtWidgets.QMainWindow, UI_MagPy4):
         self.ui = UI_MagPy4()
         self.ui.setupUI(self)
 
-        self.ui.comboBox.currentIndexChanged.connect(self.comboChange)
-        self.ui.drawStyleCombo.currentIndexChanged.connect(self.setLineStyle)
+        
         #self.ui.timeSlider.startValueChanged.connect(self.onStartChanged)
         #self.ui.timeSlider.endValueChanged.connect(self.onEndChanged)
         self.ui.startSlider.valueChanged.connect(self.onStartChanged)
@@ -48,58 +47,46 @@ class MagPy4Window(QtWidgets.QMainWindow, UI_MagPy4):
 
         #self.ui.actionTest.triggered.connect(self.plotFFDataTest)
         self.ui.actionTest.triggered.connect(self.openTracer)
-        self.jTest = 0
+        self.ui.actionOpen.triggered.connect(self.openFileDialog)
 
-        self.openFile('mmsTestData/L2/merged/2015/09/27/mms15092720')
-        self.setupSliders()
         self.markerStyle = ','
         self.lineStyle = ''
-        #self.plotFFData()
 
-    def comboChange(self, i):
-        print(f'combo changed to: {self.ui.comboBox.itemText(i)}')
+        starterFile = 'mmsTestData/L2/merged/2015/09/27/mms15092720'
+        if os.path.exists(starterFile+'.ffd'):
+            self.openFile(starterFile)
+            self.plotDataDefault()
+
 
     def openTracer(self):
         self.tracer = AxisTracer(self)
         self.tracer.show()
 
-    def setLineStyle(self, i):
-        style = self.ui.drawStyleCombo.itemText(i)
-        if style == 'dots':
-            self.markerStyle = ','
-            self.lineStyle = ''
-        elif style == 'lines':
-            self.markerStyle = ''
-            self.lineStyle = '-'
-        self.plotFFData()
-
-
-    def tighten(self):
-        print('hello')
-        plt.tight_layout()
-
-    def halfAxes(self):
-        axes = self.ui.figure.axes
-        self.tO += (self.tE-self.tO)/2
-        for ax in axes:
-            xmin,xmax,ymin,ymax = ax.axis()
-            ax.axis(xmin = self.tO,xmax=self.tE, ymin=ymin, ymax=ymax)
-        self.ui.canvas.draw()
-
     def resizeEvent(self, event):
         print('resize detected')
         plt.tight_layout()
 
+    def openFileDialog(self):
+        fileName = QtWidgets.QFileDialog.getOpenFileName(self, caption="Open Flatfile", options = QtWidgets.QFileDialog.ReadOnly, filter='Flatfiles (*.ffd)')[0]
+        if fileName is "":
+            print('OPEN FILE FAILED')
+            return
+        fileName = fileName.rsplit(".", 1)[0]
+        if fileName is None:
+            print('OPEN FILE FAILED (split)')
+            return
+        if self.FID is not None:
+            self.FID.close()
+
+        self.openFile(fileName) # first elem of tuple is filepath
+        self.plotDataDefault()
+
     def openFile(self, PATH=None):  # slot when Open pull down is selected
-        fFile = PATH
-        FID = FF_ID(fFile, status=FF_STATUS.READ | FF_STATUS.EXIST)
+        FID = FF_ID(PATH, status=FF_STATUS.READ | FF_STATUS.EXIST)
         if not FID:
             WARNING(self, "NOT HAPPENING")
             return -1, "BAD"
-        #if self.FID is not None:
-        #if hasattr(self, FID):
-            #self.FID.close()
-            #self._NoData()   # purge old panels
+
         self.FID = FID
         self.epoch = self.FID.getEpoch()
         print(f'epoch: {self.epoch}')
@@ -112,7 +99,6 @@ class MagPy4Window(QtWidgets.QMainWindow, UI_MagPy4):
             return err, mess
         self.fTime = [info["FIRST_TIME"].info, info["LAST_TIME"].info]
         self.resolution = self.FID.getResolution()
-        print(f'resolution: {self.resolution}')
 #       self.numpoints = min(self.numpoints, self.FID.FFParm["NROWS"].value)
 
         self.UTCO = FFTIME(self.times[0], Epoch=self.epoch)
@@ -125,23 +111,25 @@ class MagPy4Window(QtWidgets.QMainWindow, UI_MagPy4):
         self.tO = self.times[self.iO]
         self.tE = self.times[self.iE]
 
+        print(f'resolution: {self.resolution}')
         print(f'iO: {self.iO}')
         print(f'iE: {self.iE}')
         print(f'tO: {self.tO}')
         print(f'tE: {self.tE}')
         
-        return 1, "FILE " + fFile + "read"
+        self.setupSliders()
+
+        return 1, "FILE " + PATH + "read"
 
     def loadFile(self):
         if self.FID is None:
-            print("PROBLEM", self.ffFile)
-            quit()
+            print("Error in loadFile (not opened yet I think)")
         nRows = self.FID.getRows()
         records = self.FID.DID.sliceArray(row=1, nRow=nRows)
         self.times = records["time"]
         self.dataByRec = records["data"]
         self.dataByCol = arrayToColumns(records["data"])
-        self.data = self.dataByCol    # for FFSpectrar
+        #self.data = self.dataByCol    # for FFSpectrar
         self.epoch = self.FID.getEpoch()
 
         magData = [] # can get rid of this eventually with DATADICT holding all fetched data
@@ -167,7 +155,6 @@ class MagPy4Window(QtWidgets.QMainWindow, UI_MagPy4):
         self.stats16 = magStats
         self.magData = magData
         self.BLMData = BLMData
-        self.stats = self.condenseStats(magStats)   # merge the four spacecraft to one
         self.BLMStats = BLMStats
 #       self.fillFileStat()
         #self.updateFileStats()
@@ -245,152 +232,6 @@ class MagPy4Window(QtWidgets.QMainWindow, UI_MagPy4):
 
         self.ui.canvas.draw()
 
-    def condenseStats(self, stats, NVectors=4):  # redo stats 4 (bx,by,bz,bt)
-        functions = (np.amin, np.amin, np.amax, np.amin, np.amax, np.amin)
-        nStats = len(stats)
-        l = [0] * NVectors
-        for i in range(NVectors):
-            l[i] = []
-            for j in range(NVectors):
-                index = j * 4 + i + 1
-                l[i].append(index)
-        newStats = np.zeros([nStats, 5])
-        for i in range(nStats):
-            stat = stats[i]
-            newStats[i, 0] = 0
-            for j in range(NVectors):
-                valuesList = [stat[v] for v in l[j]]
-                newValue = functions[i](valuesList)
-                newStats[i, j + 1] = newValue
-        return newStats
-
-    def plotFFData(self):
-        self.ui.figure.clear()
-        colors = ['r','g','b','black']
-
-        #Xd = FFTIME.list2Date(self.times)
-
-        numCraft = 4
-        loc = tckr.AutoMinorLocator(5)
-        formatter = tckr.FuncFormatter(self.tickFormat)
-        #majorLoc = tckr.MaxNLocator(nbins=5,integer=False,prune=None)
-
-        fixedTics = []
-        rng = self.tE - self.tO
-        tickCount = 10
-        for i in range(tickCount+1):
-            n = self.tO + i * (rng/tickCount)
-            fixedTics.append(n)
-
-        majorLoc = tckr.FixedLocator(fixedTics)
-
-        for j in range(4):   # bx, by, bz, bt (offset into array kinda)
-            ax = self.ui.figure.add_subplot(numCraft, 1, j+1)
-
-            #ax.autoscale(enable=True, axis='y',tight=None)
-            ax.set_ymargin(0.05) # margin inside ax plot
-
-            ax.xaxis.set_minor_locator(loc)
-            #formatter = mdates.DateFormatter('%d')
-            ax.xaxis.set_major_formatter(formatter)
-
-            ax.xaxis.set_major_locator(majorLoc)
-            plt.tick_params(which='major', length = 7)
-
-            for i in range(numCraft): # for each spacecraft
-                col = i * 4 + j
-                Y = np.array(self.magData[col])
-                if len(Y) <= 1:
-                    continue
-                ax.plot(self.times, Y, marker=self.markerStyle, linestyle=self.lineStyle, lw=0.5, color = colors[i]) #snap=True, 
-
-            # draw horizontal line if crosses zero
-            ymin,ymax = plt.ylim()
-            if ymin < 0 and ymax > 0:
-                ax.axhline(color='r', lw=0.5, dashes=[5,5])
-
-            # move to fit current time
-            xmin,xmax,ymin,ymax = ax.axis()
-            ax.axis(xmin = self.tO, xmax= self.tE, ymin=ymin, ymax=ymax)
-
-            if j != numCraft-1: # have x axis labels only on bottom
-                ax.tick_params(labelbottom='off')  
-
-        #ax = self.ui.figure.add_subplot(numCraft+1, 1, 5)
-        #self.timeSlider = Slider(ax, 'time', self.tO, self.tE)
-        #self.timeSlider.on_changed(self.updateAxes)
-
-        # refresh canvas
-        self.ui.canvas.draw()
-
-
-    def plotFFDataTest(self):
-        #self.ui.figure.clear()
-        colors = ['r','g','b','black']
-        j = self.jTest
-        self.jTest += 1
-        if self.jTest > 3:
-            self.jTest = 0
-        #Xd = FFTIME.list2Date(self.times)
-
-        numCraft = 4
-        loc = tckr.AutoMinorLocator(5)
-        formatter = tckr.FuncFormatter(self.tickFormat)
-        #majorLoc = tckr.MaxNLocator(nbins=5,integer=False,prune=None)
-
-        fixedTics = []
-        rng = self.tE - self.tO
-        tickCount = 10
-        for i in range(tickCount+1):
-            n = self.tO + i * (rng/tickCount)
-            fixedTics.append(n)
-
-        majorLoc = tckr.FixedLocator(fixedTics)
-
-        axxs = self.ui.figure.axes
-        if len(axxs) == 0:
-            ax = self.ui.figure.add_subplot(numCraft, 1, j+1)
-        else:
-            ax = axxs[0]
-            ax.clear()
-
-        #ax.autoscale(enable=True, axis='y',tight=None)
-        ax.set_ymargin(0.05) # margin inside ax plot
-
-        ax.xaxis.set_minor_locator(loc)
-        #formatter = mdates.DateFormatter('%d')
-        ax.xaxis.set_major_formatter(formatter)
-
-        ax.xaxis.set_major_locator(majorLoc)
-        plt.tick_params(which='major', length = 7)
-
-        for i in range(numCraft): # for each spacecraft
-            col = i * 4 + j
-            Y = np.array(self.magData[col])
-            if len(Y) <= 1:
-                continue
-            ax.plot(self.times, Y, marker=self.markerStyle, linestyle=self.lineStyle, lw=0.5, color = colors[i]) #snap=True, 
-
-        # draw horizontal line if crosses zero
-        ymin,ymax = plt.ylim()
-        if ymin < 0 and ymax > 0:
-            ax.axhline(color='r', lw=0.5, dashes=[5,5])
-
-        # move to fit current time
-        xmin,xmax,ymin,ymax = ax.axis()
-        ax.axis(xmin = self.tO, xmax= self.tE, ymin=ymin, ymax=ymax)
-
-        if j != numCraft-1: # have x axis labels only on bottom
-            ax.tick_params(labelbottom='off')  
-
-        #ax = self.ui.figure.add_subplot(numCraft+1, 1, 5)
-        #self.timeSlider = Slider(ax, 'time', self.tO, self.tE)
-        #self.timeSlider.on_changed(self.updateAxes)
-        plt.tight_layout()
-
-        # refresh canvas
-        self.ui.canvas.draw()
-
     # x is value of tick
     # pos is position of tick int time array i think
     def tickFormat(self, x, pos):
@@ -403,7 +244,20 @@ class MagPy4Window(QtWidgets.QMainWindow, UI_MagPy4):
             t = t.split('.',1)[0]
         return t
 
-    def plotData(self, cbMatrix):
+    # setup default 4 axis magdata plot
+    def plotDataDefault(self):
+        boolMatrix = []
+        for a in range(4):
+            boolAxis = []
+            for i in range(len(DATASTRINGS)):
+                boolAxis.append(False)
+            boolMatrix.append(boolAxis)
+        for i in range(16):
+            boolMatrix[int(i/4)][i] = True
+        self.plotData(boolMatrix)
+
+    # boolMatrix is same shape as the checkBox matrix but just bools
+    def plotData(self, bMatrix):
         self.ui.figure.clear()
 
         colors = ['r','g','b','black']
@@ -421,9 +275,9 @@ class MagPy4Window(QtWidgets.QMainWindow, UI_MagPy4):
         loc = tckr.AutoMinorLocator(5)
         formatter = tckr.FuncFormatter(self.tickFormat)
 
-        numAxes = len(cbMatrix)
+        numAxes = len(bMatrix)
         plotCount = max(numAxes,4) # always space for at least 4 plots on screen
-        for ai,cbAxis in enumerate(cbMatrix):
+        for ai,bAxis in enumerate(bMatrix):
             ax = self.ui.figure.add_subplot(plotCount, 1, ai+1)
             
             ax.set_ymargin(0.05) # margin inside ax plot
@@ -435,8 +289,8 @@ class MagPy4Window(QtWidgets.QMainWindow, UI_MagPy4):
             # add traces for each data checked for this axis
             traces = 0 # number of traces on this axis
             axisString = ''
-            for i,cb in enumerate(cbAxis):
-                if cb.isChecked():
+            for i,b in enumerate(bAxis):
+                if b:
                     #print(f'{DATASTRINGS[i]}')
                     dstr = DATASTRINGS[i]
                     Y = DATADICT[dstr]
@@ -483,6 +337,7 @@ class AxisTracer(QtWidgets.QFrame, UI_AxisTracer):
         self.ui.setupUI(self)
         self.axisCount = 0
 
+        self.ui.drawStyleCombo.currentIndexChanged.connect(self.setLineStyle)
         self.ui.clearButton.clicked.connect(self.clearCheckBoxes)
         self.ui.addAxisButton.clicked.connect(self.addAxis)
         self.ui.removeAxisButton.clicked.connect(self.removeAxis)
@@ -497,8 +352,24 @@ class AxisTracer(QtWidgets.QFrame, UI_AxisTracer):
         for i in range(16):
             self.checkBoxes[int(i/4)][i].setChecked(True)            
 
+    
+    def setLineStyle(self, i):
+        style = self.ui.drawStyleCombo.itemText(i)
+        if style == 'dots':
+            self.window.markerStyle = ','
+            self.window.lineStyle = ''
+        elif style == 'lines':
+            self.window.markerStyle = ''
+            self.window.lineStyle = '-'
+
     def plotData(self):
-        self.window.plotData(self.checkBoxes)
+        boolMatrix = []
+        for cbAxis in self.checkBoxes:
+            boolAxis = []
+            for cb in cbAxis:
+                boolAxis.append(cb.isChecked())
+            boolMatrix.append(boolAxis)
+        self.window.plotData(boolMatrix)
 
     def clearCheckBoxes(self):
         for row in self.checkBoxes:
