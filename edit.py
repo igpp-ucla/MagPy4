c9b966a96a792f02360eb9692841d2b86fb86fff
jfc3 <buckslice@gmail.com>
2018-05-03 18:18:03 -0700

    Implemented matrix multiplication on edit menu

--- edit.py
+++ edit.py
@@ -6,6 +6,8 @@ from PyQt5.QtWidgets import QSizePolicy
 import numpy as np
 from FF_Time import FFTIME
 
+import functools
+
 class EditUI(object):
     def setupUI(self, Frame):
         Frame.setWindowTitle('Edit')
@@ -13,11 +15,11 @@ class EditUI(object):
 
         layout = QtWidgets.QVBoxLayout(Frame)
 
+        horizLayout = QtWidgets.QHBoxLayout()
         self.matrixPanel = QtGui.QWidget(Frame)
 
         # matrix A setup
-        self.gridFrame = QtWidgets.QGroupBox('Matrix A',self.matrixPanel)
-        self.gridFrame.setGeometry(QtCore.QRect(10,10,250,120))
+        self.gridFrame = QtWidgets.QGroupBox('Matrix A')
         self.agrid = QtWidgets.QGridLayout(self.gridFrame)
         self.A = []
         for y in range(3):
@@ -26,14 +28,12 @@ class EditUI(object):
                 lineEdit = QtGui.QLineEdit()
                 lineEdit.setInputMethodHints(QtCore.Qt.ImhFormattedNumbersOnly)
                 lineEdit.setText('0.0')
-                lineEdit.setSizePolicy(QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum))
                 self.agrid.addWidget(lineEdit, y, x, 1, 1)
                 row.append(lineEdit)
             self.A.append(row)
 
         # operator setup
-        self.opFrame = QtWidgets.QGroupBox('Operators',self.matrixPanel)
-        self.opFrame.setGeometry(QtCore.QRect(265,10,80,120))
+        self.opFrame = QtWidgets.QGroupBox('Operators')
         self.opLayout = QtWidgets.QVBoxLayout(self.opFrame)
         self.upload = QtGui.QPushButton('A => R')
         self.operate = QtGui.QPushButton('AxR => R')
@@ -43,43 +43,106 @@ class EditUI(object):
         self.opLayout.addWidget(self.download)
 
         # matrix R setup
-        self.gridFrame = QtWidgets.QGroupBox('Rotation Matrix',self.matrixPanel)
-        self.gridFrame.setGeometry(QtCore.QRect(350,10,250,120))
-        self.rgrid = QtWidgets.QGridLayout(self.gridFrame)
+        self.rotFrame = QtWidgets.QGroupBox('Rotation Matrix')
+        self.rgrid = QtWidgets.QGridLayout(self.rotFrame)
         self.R = []
         for y in range(3):
             row = []
             for x in range(3):
                 label = QtGui.QLabel()
                 label.setText('0.0')
-                label.setSizePolicy(QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum))
                 self.rgrid.addWidget(label, y, x, 1, 1)
                 row.append(label)
             self.R.append(row)
 
+        horizLayout.addWidget(self.gridFrame,2)
+        horizLayout.addWidget(self.opFrame,1)
+        horizLayout.addWidget(self.rotFrame,2)
 
         self.buttonBox = QtWidgets.QDialogButtonBox(Frame)
         self.buttonBox.setLayoutDirection(QtCore.Qt.RightToLeft)
-        self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Apply|QtGui.QDialogButtonBox.Reset)
+        self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Apply | QtGui.QDialogButtonBox.Reset)
 
-        layout.addWidget(self.matrixPanel)
+        layout.addLayout(horizLayout)
+
+        self.identity = QtGui.QPushButton('Load Identity')
+        self.identity.setSizePolicy(QSizePolicy(QSizePolicy.Maximum, QSizePolicy.Minimum))
+        layout.addWidget(self.identity)
+
+        layout.addStretch()
         layout.addWidget(self.buttonBox)
 
-        # testing 
+        # testing
         self.A[0][2].setText('1.0')
         self.A[1][0].setText('2.0')
 
         #layout.addWidget(self.gridFrame)
         #layout.addWidget(self.opFrame)
-
-
 class Edit(QtWidgets.QFrame, EditUI):
+
     def __init__(self, window, parent=None):
         super(Edit, self).__init__(parent)
         self.window = window
         self.ui = EditUI()
         self.ui.setupUI(self)
-        
-        #self.ui.updateButton.clicked.connect(self.updateSpectra)
-        #self.ui.bandWidthSpinBox.valueChanged.connect(self.updateSpectra)
-        #self.ui.oneTracePerCheckBox.stateChanged.connect(self.updateSpectra)
+
+        self.i = [0, 1, 2]
+        self.IDENTITY = [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]
+
+        self.ui.upload.clicked.connect(self.upload)
+        self.ui.operate.clicked.connect(self.operate)
+        self.ui.download.clicked.connect(self.download)
+        self.ui.identity.clicked.connect(functools.partial(self.setMatrix, self.ui.A, self.IDENTITY))
+
+    def empty(self): # return new 2D list
+        return [[0.0,0.0,0.0],[0.0,0.0,0.0],[0.0,0.0,0.0]]
+
+    # returns a copy of array a
+    def copy(self, a):
+        return [[a[0][0],a[0][1],a[0][2]],
+                [a[1][0],a[1][1],a[1][2]],
+                [a[2][0],a[2][1],a[2][2]]]
+
+    # mat is 2D list of label/lineEdits
+    def getMatrix(self, mat):
+        M = self.empty()
+        for i in self.i:
+            for j in self.i:
+                s = mat[i][j].text()
+                try:
+                    f = float(s)
+                except ValueError:
+                    print(f'matrix has non-number at location {i},{j}')
+                    f = 0.0
+                M[i][j] = f
+
+        #for r in M:
+        #    print(f'[{r[0]}][{r[1]}][{r[2]}]')
+
+        return M
+
+    # mat is 2D list of label/lineEdits, m is 2D list of floats
+    def setMatrix(self, mat, m):
+        for i in self.i:
+            for j in self.i:
+                n = m[i][j]
+                if n > 9999.9999:
+                    mat[i][j].setText(f'{n:.4e}')
+                else:
+                    mat[i][j].setText(f'{n:.4f}')
+
+    def upload(self):
+        self.setMatrix(self.ui.R, self.getMatrix(self.ui.A))
+
+    def download(self):
+        self.setMatrix(self.ui.A, self.getMatrix(self.ui.R))
+
+    def operate(self):
+        A = self.getMatrix(self.ui.A)
+        R = self.getMatrix(self.ui.R)
+        N = self.empty()
+        for r in self.i:
+            for c in self.i:
+                for i in self.i:
+                    N[r][c] += A[r][i] * R[i][c]
+        self.setMatrix(self.ui.R, N)
