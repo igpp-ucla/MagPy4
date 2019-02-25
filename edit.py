Corrected instToSC rotation to use degrees instead of radians

edit.py
@@ -421,7 +421,7 @@ class CustomRot(QtWidgets.QFrame, CustomRotUI):
         self.ui.R.setMatrix(self.edit.ui.M.getMatrix())
 
     def loadInstrToSpaceMat(self):
-        theta = 57.9
+        theta = 57.9 * self.D2R
         mat = np.array([[cos(theta), -sin(theta), 0],
                         [sin(theta), cos(theta), 0],
                         [0, 0, 1]])