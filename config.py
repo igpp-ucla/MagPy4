07e4cd4a31ba0d154a6a1992ef0590e675e5f0c5
pbravo-igpp <pbravo@igpp.ucla.edu>
2021-07-01 13:15:06 -0700

    Merged build changes into master

---
+++ config.py
@@ -0,0 +1,35 @@
+config_str = """\
+[Application]
+name=MagPy
+version={0}
+entry_point=MagPy4.MagPy4:runMagPy
+console=true
+icon=MagPy4/images/magPy_blue.ico
+[Python]
+version=3.8.6
+[Include]
+packages=
+    tkinter
+    _tkinter
+pypi_wheels = {1}
+extra_wheel_sources =
+    fflib
+    pip_wheels
+files=lib
+exclude=
+    MagPy4/testData\
+"""
+
+def main():
+    with open('MagPy4/version.txt', 'r') as fd:
+        version = fd.read().strip('\n')
+    
+    with open('requirements.txt', 'r') as fd:
+        requirements = fd.readlines()
+        requirements = '\t'.join(requirements).rstrip('\n')
+    
+    txt = config_str.format(version, requirements)
+    with open('installer.cfg', 'w') as fd:
+        fd.write(txt)
+
+main()
