f92f9cf4ff28dfc65f4e26211846140f3d462300
pbravo-igpp <pbravo@igpp.ucla.edu>
2021-07-13 14:28:21 -0700

    Updated alg module to allow plotting onto existing axes

--- MagPy4/alg/alg.py
+++ MagPy4/alg/alg.py
@@ -454,8 +454,29 @@ def _log_formatter(pos, val):
         pos = np.round(pos, 3)
     return f'$10^{{{pos}}}$'
 
+def create_spec_object(x, y, z):
+    '''
+        Generates a SpecData object from the given bins 
+        and grid values
+
+        Parameters
+        ----------
+        x : array-like (m,)
+            List of datetimes representing x-bins
+        y : array-like (n,)
+            List of floats representing the y-bins
+        z : array-like (m-1, n-1,)
+            Grid of floats representing the values
+            in the grid
+    '''
+    x = np.array(x)
+    y = np.array(y)
+    z = np.array(z)
+    return SpecData(y, x, z)
+
 def plot_spec_data(spec, figsize=(9, 6), title=None, logy=None,
-                cmap=None, rng=None, logcolor=None):
+                cmap=None, rng=None, logcolor=None, smooth=False,
+                fig_ax=None):
     ''' 
         Plots a SpecData object and onto a matplotlib Figure
 
@@ -483,19 +504,29 @@ def plot_spec_data(spec, figsize=(9, 6), title=None, logy=None,
                     Specifies whether grid values should be mapped to log10
                     values before mapping to color values;
                     Default value is None, which will use the spec's value
+        smooth : boolean or None, optional
+                    Indicates whether to smooth the blocks in the image
+                    Defaults to False
+        fig_ax : tuple (matplotlib Figure, matplotlib Axes), optional
+                    If this is not None, the plot will be generated on the
+                    axis item in the given figure instead of a new figure
+
         Returns
         ---------
-        Figure object
+        A tuple - (Figure object, Axes object, Colorbar object)
     '''
     # Get any missing parameters from SpecData
     if title is None:
         title = spec.get_name()
 
     # Create figure and set title and size
-    fig, ax = plt.subplots(nrows=1)
-    fig.suptitle(title)
-    fig.canvas.set_window_title(title)
-    fig.set_size_inches(*figsize)
+    if fig_ax is None:
+        fig, ax = plt.subplots(nrows=1)
+        fig.suptitle(title)
+        fig.canvas.set_window_title(title)
+        fig.set_size_inches(*figsize)
+    else:
+        fig, ax = fig_ax
 
     # Get color range and settings
     logcolor = spec.log_color_scale() if logcolor is None else logcolor
@@ -510,6 +541,13 @@ def plot_spec_data(spec, figsize=(9, 6), title=None, logy=None,
     # Extract data values
     x, y, z = spec.values()
 
+    # Adjust points if smoothing
+    if smooth:
+        x_diff = np.diff(x)
+        y_diff = np.diff(y)
+        x = x[:-1] + (x_diff / 2)
+        y = y[:-1] + (y_diff / 2)
+
     # Map values to log scale if specified
     if logcolor:
         z = np.array(z)
@@ -520,10 +558,11 @@ def plot_spec_data(spec, figsize=(9, 6), title=None, logy=None,
 
     # Plot grid colors
     cmap = spec.cmap if cmap is None else cmap
-    img = ax.pcolormesh(x, y, z, cmap=cmap, vmin=vmin, vmax=vmax)
+    shading = 'flat' if not smooth else 'gouraud'
+    img = ax.pcolormesh(x, y, z, cmap=cmap, vmin=vmin, vmax=vmax, shading=shading)
 
     # Set up color bar
-    bar = fig.colorbar(img)
+    bar = fig.colorbar(img, ax=ax, pad=0.03, aspect=17)
     if logcolor:
         bar.ax.yaxis.set_major_formatter(matplotlib.ticker.FuncFormatter(_log_formatter))
 
@@ -541,15 +580,17 @@ def plot_spec_data(spec, figsize=(9, 6), title=None, logy=None,
     ax.tick_params(top=True, right=True)
     ax.tick_params(length=4)
 
-    # Add 'Time Range' text
-    fmt = '%Y %b %d %H:%M:%S.%f'
-    start_str = x[0].item().strftime(fmt)
-    end_str = x[-1].item().strftime(fmt)
-    time_lbl = f'Time Range: {start_str} to {end_str}'
-    fig.text(0.08, 0.015, time_lbl)
+    # Additional adjusments to figure if fig_ax not passed
+    if fig_ax is None:
+        # Add 'Time Range' text
+        fmt = '%Y %b %d %H:%M:%S.%f'
+        start_str = x[0].item().strftime(fmt)
+        end_str = x[-1].item().strftime(fmt)
+        time_lbl = f'Time Range: {start_str} to {end_str}'
+        fig.text(0.08, 0.015, time_lbl)
 
-    # Adjust figure layout
-    fig.tight_layout(rect=[0, 0.03, 1, 0.97])
-    fig.subplots_adjust(top=0.93)
+        # Adjust figure layout
+        fig.tight_layout(rect=[0, 0.03, 1, 0.97])
+        fig.subplots_adjust(top=0.93)
 
-    return fig
+    return fig, ax, bar
